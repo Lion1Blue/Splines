7b1000252a90caeafceb7b8b322020ae8ff0c412
Lion1Blue <58215811+Lion1Blue@users.noreply.github.com>
2021-04-22 09:10:31 +0200

    Add files via upload

---
+++ Splines/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace Splines
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewSplines = new System.Windows.Forms.DataGridView();
+            this.plotViewSplines = new OxyPlot.WindowsForms.PlotView();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.checkBoxControlLine = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelPolyDegreeOpenBSpline = new System.Windows.Forms.Label();
+            this.trackBarOpenUniformBSpline = new System.Windows.Forms.TrackBar();
+            this.radioButtonOpenUniformBSpline = new System.Windows.Forms.RadioButton();
+            this.radioButtonCatmullRom = new System.Windows.Forms.RadioButton();
+            this.radioButtonBezier = new System.Windows.Forms.RadioButton();
+            this.labelError = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSplines)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarOpenUniformBSpline)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridViewSplines
+            // 
+            this.dataGridViewSplines.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSplines.Location = new System.Drawing.Point(11, 10);
+            this.dataGridViewSplines.Name = "dataGridViewSplines";
+            this.dataGridViewSplines.RowTemplate.Height = 25;
+            this.dataGridViewSplines.Size = new System.Drawing.Size(272, 228);
+            this.dataGridViewSplines.TabIndex = 0;
+            // 
+            // plotViewSplines
+            // 
+            this.plotViewSplines.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.plotViewSplines.Location = new System.Drawing.Point(324, 12);
+            this.plotViewSplines.Name = "plotViewSplines";
+            this.plotViewSplines.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotViewSplines.Size = new System.Drawing.Size(734, 598);
+            this.plotViewSplines.TabIndex = 1;
+            this.plotViewSplines.Text = "plotView1";
+            this.plotViewSplines.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotViewSplines.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotViewSplines.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(11, 244);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(75, 23);
+            this.buttonDraw.TabIndex = 2;
+            this.buttonDraw.Text = "Draw";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(92, 244);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // checkBoxControlLine
+            // 
+            this.checkBoxControlLine.AutoSize = true;
+            this.checkBoxControlLine.Location = new System.Drawing.Point(173, 247);
+            this.checkBoxControlLine.Name = "checkBoxControlLine";
+            this.checkBoxControlLine.Size = new System.Drawing.Size(88, 19);
+            this.checkBoxControlLine.TabIndex = 4;
+            this.checkBoxControlLine.Text = "ControlLine";
+            this.checkBoxControlLine.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelPolyDegreeOpenBSpline);
+            this.groupBox1.Controls.Add(this.trackBarOpenUniformBSpline);
+            this.groupBox1.Controls.Add(this.radioButtonOpenUniformBSpline);
+            this.groupBox1.Controls.Add(this.radioButtonCatmullRom);
+            this.groupBox1.Controls.Add(this.radioButtonBezier);
+            this.groupBox1.Location = new System.Drawing.Point(11, 273);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(272, 119);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Splines";
+            // 
+            // labelPolyDegreeOpenBSpline
+            // 
+            this.labelPolyDegreeOpenBSpline.AutoSize = true;
+            this.labelPolyDegreeOpenBSpline.Location = new System.Drawing.Point(123, 76);
+            this.labelPolyDegreeOpenBSpline.Name = "labelPolyDegreeOpenBSpline";
+            this.labelPolyDegreeOpenBSpline.Size = new System.Drawing.Size(50, 15);
+            this.labelPolyDegreeOpenBSpline.TabIndex = 7;
+            this.labelPolyDegreeOpenBSpline.Text = "Degree: ";
+            // 
+            // trackBarOpenUniformBSpline
+            // 
+            this.trackBarOpenUniformBSpline.LargeChange = 1;
+            this.trackBarOpenUniformBSpline.Location = new System.Drawing.Point(174, 71);
+            this.trackBarOpenUniformBSpline.Maximum = 5;
+            this.trackBarOpenUniformBSpline.Minimum = 1;
+            this.trackBarOpenUniformBSpline.Name = "trackBarOpenUniformBSpline";
+            this.trackBarOpenUniformBSpline.Size = new System.Drawing.Size(96, 45);
+            this.trackBarOpenUniformBSpline.TabIndex = 6;
+            this.trackBarOpenUniformBSpline.Value = 2;
+            // 
+            // radioButtonOpenUniformBSpline
+            // 
+            this.radioButtonOpenUniformBSpline.AutoSize = true;
+            this.radioButtonOpenUniformBSpline.Location = new System.Drawing.Point(7, 72);
+            this.radioButtonOpenUniformBSpline.Name = "radioButtonOpenUniformBSpline";
+            this.radioButtonOpenUniformBSpline.Size = new System.Drawing.Size(97, 19);
+            this.radioButtonOpenUniformBSpline.TabIndex = 5;
+            this.radioButtonOpenUniformBSpline.Text = "open B Spline";
+            this.radioButtonOpenUniformBSpline.UseVisualStyleBackColor = true;
+            this.radioButtonOpenUniformBSpline.Enter += new System.EventHandler(this.radioButtonOpenUniformBSpline_Enter);
+            // 
+            // radioButtonCatmullRom
+            // 
+            this.radioButtonCatmullRom.AutoSize = true;
+            this.radioButtonCatmullRom.Location = new System.Drawing.Point(7, 49);
+            this.radioButtonCatmullRom.Name = "radioButtonCatmullRom";
+            this.radioButtonCatmullRom.Size = new System.Drawing.Size(95, 19);
+            this.radioButtonCatmullRom.TabIndex = 1;
+            this.radioButtonCatmullRom.Text = "Catmull Rom";
+            this.radioButtonCatmullRom.UseVisualStyleBackColor = true;
+            this.radioButtonCatmullRom.Enter += new System.EventHandler(this.radioButtonCatmullRom_Enter);
+            // 
+            // radioButtonBezier
+            // 
+            this.radioButtonBezier.AutoSize = true;
+            this.radioButtonBezier.Checked = true;
+            this.radioButtonBezier.Location = new System.Drawing.Point(7, 23);
+            this.radioButtonBezier.Name = "radioButtonBezier";
+            this.radioButtonBezier.Size = new System.Drawing.Size(56, 19);
+            this.radioButtonBezier.TabIndex = 0;
+            this.radioButtonBezier.TabStop = true;
+            this.radioButtonBezier.Text = "Bezier";
+            this.radioButtonBezier.UseVisualStyleBackColor = true;
+            this.radioButtonBezier.Enter += new System.EventHandler(this.radioButtonBezier_Enter);
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelError.ForeColor = System.Drawing.Color.Red;
+            this.labelError.Location = new System.Drawing.Point(15, 401);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(238, 50);
+            this.labelError.TabIndex = 6;
+            this.labelError.Text = "Catmull-Rom Spline must\r\nconsist of min. 4 Points";
+            this.labelError.Visible = false;
+            this.labelError.Click += new System.EventHandler(this.labelError_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridViewSplines);
+            this.panel1.Controls.Add(this.labelError);
+            this.panel1.Controls.Add(this.buttonDraw);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.buttonClear);
+            this.panel1.Controls.Add(this.checkBoxControlLine);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(294, 600);
+            this.panel1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 628);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.plotViewSplines);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSplines)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarOpenUniformBSpline)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewSplines;
+        private OxyPlot.WindowsForms.PlotView plotViewSplines;
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.CheckBox checkBoxControlLine;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonCatmullRom;
+        private System.Windows.Forms.RadioButton radioButtonBezier;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.Label labelPolyDegreeOpenBSpline;
+        private System.Windows.Forms.TrackBar trackBarOpenUniformBSpline;
+        private System.Windows.Forms.RadioButton radioButtonOpenUniformBSpline;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Splines/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+using System.Diagnostics;
+
+namespace Splines
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridViewSplines.ColumnCount = 2;
+            dataGridViewSplines.RowCount = 1;
+            dataGridViewSplines.Columns[0].HeaderCell.Value = "x";
+            dataGridViewSplines.Columns[1].HeaderCell.Value = "y";
+            labelPolyDegreeOpenBSpline.Text = $"Degree: {trackBarOpenUniformBSpline.Value}";
+
+            //Creating a Poltmodel
+            myPlotModel = new PlotModel { Title = "Splines" };
+            myPlotModel.Series.Add(new LineSeries());
+            myPlotModel.Series.Add(new LineSeries());
+            plotViewSplines.Model = myPlotModel;
+
+            DrawAll();
+            InitalizeMouseEvents(myPlotModel);
+
+            plotViewSplines.MouseUp += PlotViewSplines_MouseUp;
+        }
+
+        int indexOfCurrentSelectedPoint = -1;
+        bool onMouseHold = false;
+
+        private void PlotViewSplines_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && onMouseHold)
+            {
+                onMouseHold = false;
+            }
+        }
+
+        public Point[] GetPointsFromGrid(DataGridView dataGridView)
+        {
+            Point[] points = new Point[dataGridView.RowCount - 1];
+
+            for (int i = 0; i < dataGridView.RowCount - 1; i++)
+            {
+                double x = Convert.ToDouble(dataGridView.Rows[i].Cells[0].Value);
+                double y = Convert.ToDouble(dataGridView.Rows[i].Cells[1].Value);
+
+                points[i] = new Point(x, y);
+            }
+
+            return points;
+        }
+
+        
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            DrawAll();
+        }
+
+        PlotModel myPlotModel;
+
+        private void DrawAll()
+        {
+            Point[] points = GetPointsFromGrid(dataGridViewSplines);
+            LineSeries controlPoints = new LineSeries();
+            controlPoints.Title = "ControlPoints";
+            controlPoints.LineStyle = checkBoxControlLine.Checked ? LineStyle.Dot : LineStyle.None;
+            controlPoints.MarkerType = MarkerType.Circle;
+            controlPoints.Color = OxyColors.Red;
+
+            DrawSpline(myPlotModel);
+            DrawControlPoints(myPlotModel, controlPoints, points);
+
+            myPlotModel.InvalidatePlot(true);
+        }
+
+        public delegate Point SplineFunction(double t, Point[] points, int k);
+        private void DrawSpline(PlotModel myModel)
+        {
+            Point[] points = GetPointsFromGrid(dataGridViewSplines);
+
+            LineSeries splineLine = new LineSeries();
+            splineLine.Title = currentSpline.ToString();
+            splineLine.MarkerType = MarkerType.None;
+            splineLine.Color = OxyColors.Blue;
+
+            if (points.Length == 0)
+            {
+                myModel.Series[0] = splineLine;
+                return;
+            }
+
+            SplineFunction splineFunction;
+            float length;
+            float start = 0;
+            float dx = 0.001f;
+            int k;
+
+            switch (currentSpline)
+            {
+                case SplineType.Bezier:
+                    splineFunction = Spline.Bezier;
+                    length = 1;
+                    start = 0;
+                    dx = 0.001f;
+                    k = points.Length - 1;
+                    break;
+
+                case SplineType.CatmullRom:
+                    if (points.Length < 4)
+                    {
+                        labelError.Visible = true;
+                        return;
+                    }
+                    labelError.Visible = false;
+                    k = 0;
+                    splineFunction = Spline.CatmullRom;
+                    length = points.Length - 3f;
+                    start = 0;
+                    dx = 0.005f;
+                    break;
+
+                case SplineType.OpenUniformBSpline:
+                    splineFunction = Spline.OpenUniformBSpline;
+                    start = 0;
+                    length = points.Length - trackBarOpenUniformBSpline.Value + 2;
+                    dx = 0.005f;
+                    k = trackBarOpenUniformBSpline.Value;
+                    break;
+
+                default:
+                    return;
+            }
+
+            for (float t = start; t <= length; t += dx)
+            {
+                Point a = splineFunction(t, points, k);
+                if (a != null)
+                {
+                    splineLine.Points.Add(new DataPoint(a.X, a.Y));
+                }
+            }
+
+            myModel.Series[0] = splineLine;
+        }
+        private void InitalizeMouseEvents(PlotModel myModel)
+        {
+            Point[] points = GetPointsFromGrid(dataGridViewSplines);
+            LineSeries controlPoints = new LineSeries();
+            controlPoints.Title = "ControlPoints";
+            controlPoints.LineStyle = checkBoxControlLine.Checked ? LineStyle.Dot : LineStyle.None;
+            controlPoints.MarkerType = MarkerType.Circle;
+            controlPoints.Color = OxyColors.Red;
+
+            myModel.MouseDown += (s, e) =>
+            {
+                double x, y;
+
+                Point[] points = GetPointsFromGrid(dataGridViewSplines);
+
+                x = controlPoints.InverseTransform(e.Position).X;
+                y = controlPoints.InverseTransform(e.Position).Y;
+
+                //Get the the distance from two points 9 pixels apart
+                ScreenPoint screenPointOne = new ScreenPoint(0, 0);
+                ScreenPoint screenPointTwo = new ScreenPoint(7, 7);
+
+                DataPoint dataPointOne = controlPoints.InverseTransform(screenPointOne);
+                DataPoint dataPointTwo = controlPoints.InverseTransform(screenPointTwo);
+
+                double dx = dataPointOne.X - dataPointTwo.X;
+                double dy = dataPointOne.Y - dataPointTwo.Y;
+
+                double distance = Math.Sqrt(dx * dx + dy * dy);
+
+                int index;
+
+                bool sucessfullGrap = IsInDistance(distance, points, new Point(x, y), out index);
+
+                if (e.ChangedButton == OxyMouseButton.Right && sucessfullGrap)
+                {
+                    dataGridViewSplines.Rows.RemoveAt(index);
+                    points = GetPointsFromGrid(dataGridViewSplines);
+                    DrawSpline(myModel);
+                    DrawControlPoints(myModel, controlPoints, points);
+                    //Updates the View
+                    myModel.InvalidatePlot(true);
+
+                    return;
+                }
+
+                if (e.ChangedButton == OxyMouseButton.Left && !sucessfullGrap)
+                {
+                    AppendPointToDataGridView(dataGridViewSplines, new Point(x, y));
+                    points = GetPointsFromGrid(dataGridViewSplines);
+                    DrawSpline(myModel);
+                    DrawControlPoints(myModel, controlPoints, points);
+                    //Updates the View
+                    myModel.InvalidatePlot(true);
+
+                    return;
+                }
+
+                if (e.ChangedButton == OxyMouseButton.Left && sucessfullGrap)
+                {
+                    onMouseHold = true;
+                    indexOfCurrentSelectedPoint = index;
+                }
+
+            };
+
+            myModel.MouseMove += (s, e) =>
+            {
+                if (onMouseHold)
+                {
+                    Point[] points = GetPointsFromGrid(dataGridViewSplines);
+
+                    double x, y;
+
+                    x = controlPoints.InverseTransform(e.Position).X;
+                    y = controlPoints.InverseTransform(e.Position).Y;
+
+                    dataGridViewSplines.Rows[indexOfCurrentSelectedPoint].Cells[0].Value = x.ToString();
+                    dataGridViewSplines.Rows[indexOfCurrentSelectedPoint].Cells[1].Value = y.ToString();
+
+                    DrawSpline(myModel);
+                    DrawControlPoints(myModel, controlPoints, points);
+                    myModel.InvalidatePlot(true);
+                }
+            };
+
+
+            trackBarOpenUniformBSpline.ValueChanged += (s, e) =>
+            {
+                labelPolyDegreeOpenBSpline.Text = $"Degree: {trackBarOpenUniformBSpline.Value}";
+
+                if (currentSpline == SplineType.OpenUniformBSpline)
+                {
+                    DrawSpline(myPlotModel);
+                    myPlotModel.InvalidatePlot(true);
+                }
+            };
+
+
+            checkBoxControlLine.CheckedChanged += (s, e) =>
+            {
+                controlPoints.LineStyle = checkBoxControlLine.Checked ? LineStyle.Dot : LineStyle.None;
+                Point[] points = GetPointsFromGrid(dataGridViewSplines);
+                DrawControlPoints(myModel, controlPoints, points);
+                myModel.InvalidatePlot(true);
+            };
+
+            DrawControlPoints(myModel, controlPoints, points);
+        }
+
+        private void AppendPointToDataGridView(DataGridView dataGridView, Point point)
+        {
+            dataGridView.RowCount++;
+            dataGridView.Rows[dataGridView.RowCount - 2].Cells[0].Value = point.X.ToString();
+            dataGridView.Rows[dataGridView.RowCount - 2].Cells[1].Value = point.Y.ToString();
+        }
+
+        private void DrawControlPoints(PlotModel myModel, LineSeries controlPoints, Point[] points)
+        {
+            controlPoints.Points.Clear();
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                controlPoints.Points.Add(new DataPoint(points[i].X, points[i].Y));
+            }
+
+            myModel.Series[1] = controlPoints;
+        }
+
+        public bool IsInDistance(double maxDistance, Point[] points, Point point, out int indexOfMin)
+        {
+            int currentIndex = 0;
+            double currentMaxDistance = maxDistance;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                double dx = points[i].X - point.X;
+                double dy = points[i].Y - point.Y;
+
+                double distance = Math.Sqrt(dx * dx + dy * dy);
+                if (currentMaxDistance > distance)
+                {
+                    currentIndex = i;
+                    currentMaxDistance = distance;
+                }
+            }
+
+            if (maxDistance == currentMaxDistance)
+            {
+                indexOfMin = 0;
+                return false;
+            }
+
+
+            indexOfMin = currentIndex;
+            return true;
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            dataGridViewSplines.RowCount = 1;
+            DrawAll();
+        }
+
+        enum SplineType { Bezier, CatmullRom, OpenUniformBSpline };
+        SplineType currentSpline = SplineType.Bezier;
+
+        private void radioButtonBezier_Enter(object sender, EventArgs e)
+        {
+            currentSpline = SplineType.Bezier;
+            labelError.Visible = false;
+            DrawAll();
+        }
+
+        private void radioButtonCatmullRom_Enter(object sender, EventArgs e)
+        {
+            currentSpline = SplineType.CatmullRom;
+            DrawAll();
+            radioButtonCatmullRom.Checked = true;
+        }
+
+        private void labelError_Click(object sender, EventArgs e)
+        {
+            labelError.Visible = false;
+        }
+
+        private void radioButtonOpenUniformBSpline_Enter(object sender, EventArgs e)
+        {
+            currentSpline = SplineType.OpenUniformBSpline;
+            DrawAll();
+        }
+    }
+}
